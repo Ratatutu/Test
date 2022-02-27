7c06d2fcaff5e169ba4c5d889418d8ea7601a095
Ratatutu <99909328+Ratatutu@users.noreply.github.com>
2022-02-27 21:53:59 +0530

    Create appp.py

---
+++ appp.py
@@ -0,0 +1,15 @@
+from flask import Flask,request,jsonify
+import os
+
+app=Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello World oaweigiwebgoaweb!"
+
+
+hostIP="0.0.0.0"
+hostPort=os.environ.get("PORT",5000)
+if(__name__=="main"):
+  print("Starting up in main...")
+  app.run(host=hostIP,port=hostPort)
